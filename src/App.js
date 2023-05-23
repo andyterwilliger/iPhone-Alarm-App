36b81344f26ff78caa36d4a1df46269852d8a9a7
andyterwilliger <76022623+andyterwilliger@users.noreply.github.com>
2023-05-22 20:31:42 -0500

    Added Header, Sleep components with corresponding CSS files. Basic styling on Header and Sleep components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
 import Header from './components/Header'
+import Sleep from './components/Sleep';
 export default function App() {
   return (
     <div>
       <Header/>
+      <Sleep/>
     </div>
   );
 }
--- src/components/Sleep.js
+++ src/components/Sleep.js
@@ -1,7 +1,13 @@
+import React from 'react';
+import './Sleep.css'
 const Sleep = () =>{
   return(
     <div className="sleep_wake_container">
       <h3>Sleep | Wake Up</h3>
+      <div className="health_set_up">
+        <p>No Alarm</p>
+        <button>SET UP</button>
+      </div>
       </div>
   )
 }
